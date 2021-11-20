41830bcd2eeae862fea84dd57603343dc32515cc
Kevin Liu <kevinliu1996@gmail.com>
2021-11-20 14:40:23 -0500

    Add .env.sample and Bot library

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import express from 'express';
+const { App } = require('@slack/bolt');
 
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('The sedulous hyena ate the antelope!');
+const app = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
 });
-app.listen(port, err => {
-    if (err) {
-        return console.error(err);
-    }
-    return console.log(`server is listening on ${port}`);
-});
+
+/* Add functionality here */
+
+(async () => {
+    // Start the app
+    await app.start(process.env.PORT || 3000);
+
+    console.log('⚡️ Bolt app is running!');
+})();
