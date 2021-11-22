5dc6e664448d26a164fe3439dd3eaf871b18c81b
Kevin Liu <kevinliu1996@gmail.com>
2021-11-22 13:54:48 -0500

    Update util.ts

--- src/util.ts
+++ src/util.ts
@@ -3,16 +3,19 @@ import { EMOJI_KEYWORDS } from './constant';
 const emojiSet = require('emoji-set');
 
 
-const nRandomElements = (n: number) => (el: any[]) =>
-  el.sort(() => 0.5 - Math.random());
+const nRandomElements = (elements: any[], n: number) => elements
+.sort(() => 0.5 - Math.random())
+.slice(0, n);
 
 // @todo: Needz more algoz
-const getHappyEmojis = (n: number) => nRandomElements(n)(
-  EMOJI_KEYWORDS.reduce((acc, keyword) => {
+const getHappyEmojis = (n: number) => {
+  const emojis = EMOJI_KEYWORDS.reduce((acc, keyword) => {
+    // ['😀', '😃', ...]
     const emojis = emojiSet.searchByKeyword(keyword, false, true);
     return acc.concat(...emojis);
-  }, []),
-);
+  }, []);
+  return nRandomElements(emojis, n);
+};
 
 export {
   getHappyEmojis,
